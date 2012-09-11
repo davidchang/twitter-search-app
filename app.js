6d5e818e3c4d121995e40777b5edb441947421b3
davidchang <dchang.me@gmail.com>
2012-09-10 20:15:37 -0700

    before moving from node-twitter to twitter npm mod

--- app.js
+++ app.js
@@ -6,8 +6,10 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , Twitter = require('node-twitter');
 
+/* EXPRESS */
 var app = express();
 
 app.configure(function(){
@@ -27,9 +29,53 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/:stream', routes.stream);
+var server = http.createServer(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+var io = require('socket.io').listen(server);
+io.configure(function() {
+	io.set('close timeout', 60 * 10);
+});
+
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+app.get('/', routes.index);
+app.get('/:stream', function(req, res) {
+  res.render('stream', { title: 'Search Twitter | ' + req.params.stream });
+  io.sockets.on('connection', function(socket) {
+  
+	var publishFlag = true;
+	setInterval(function() {
+		publishFlag = true;
+	}, 1500);
+  
+	/* TWITTER API */
+	var twitterStreamClient = new Twitter.StreamClient(
+		'DbCwANKEP9tiT3r0Eeg',
+		'NrvIoAHtkiIsBINDgXwSCZFFeozhMbBFQDU5GwLBLk',
+		'229849894-V799oSBnGKm3nrwPMFerIbEsPiGTWr3uqO2DpfzI',
+		'bbZdwtqpwseThw54iVCi45xtC5e96VFNP5nTi6apI'
+	);
+
+	twitterStreamClient.on('close', function() {
+		console.log('Connection closed.');
+	});
+	twitterStreamClient.on('end', function() {
+		console.log('End of Line.');
+	});
+	twitterStreamClient.on('error', function(error) {
+		console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+	});
+	twitterStreamClient.on('tweet', function(tweet) {
+		if(publishFlag) {
+			socket.emit('tweet', tweet);
+			publishFlag = false;
+		}
+	});
+	/* End of TWITTER API */
+	
+	twitterStreamClient.start([req.params.stream]);
+  });
+});
+/* End of EXPRESS */
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ exports.index = function(req, res){
   res.render('index', { title: 'Search Twitter' });
 };
 
+/*
 exports.stream = function(req, res) {
   res.render('stream', { title: 'Search Twitter | ' + req.params.stream });
 };
+*/
